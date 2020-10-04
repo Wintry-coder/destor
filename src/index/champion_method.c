a36661b228160b848056dfd822e04fac30b479d3
Wintry-coder <1332765311@qq.com>
2020-10-04 13:20:50 +0800

    update the method

--- src/index/champion_method.c
+++ src/index/champion_method.c
@@ -1,19 +1,30 @@
 #include "../destor.h"
 #include "context_table.h"
 
-
+#define EPSILON 0.1
 struct contextItem *(*champion_choose)(GList *contextList);
 
 static struct contextItem* champion_choose_random(GList* contextList) {
-
+    srand(time(NULL));
+    int contextList_length = g_list_length(contextList);
+    double rand_num = rand()/(RAND_MAX/1.0);
+    int contextList_length = g_list_length(ctxtList);
+    int index_ = (int) (rand_num * contextList_length);
+    return g_list_nth_data(contextList, index_);
 }
 
 static struct contextItem* champion_choose_greedy(GList* contextList) {
-
+    srand(time(NULL));
+    double rand_num = rand()/(RAND_MAX/1.0);
+    if (rand_num < 1 - EPSILON) {
+        return find_max_Item(contextList);
+    } else {
+        return champion_choose_random(contextList);
+    }
 }
 
 static struct contextItem* champion_choose_recent(GList* contextList) {
-
+    GList* p = g_list_last(contextList);
 }
 
 void init_champion_method(){
