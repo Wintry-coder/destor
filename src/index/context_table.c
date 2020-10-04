7d6ad2f5ce81ecfed7f5bb584d0aba395d85e420
Wintry-coder <1332765311@qq.com>
2020-10-04 10:13:00 +0800

    update

--- src/index/context_table.c
+++ src/index/context_table.c
@@ -4,3 +4,6 @@
  *  Created on: Oct 3, 2020
  *      Author: Yanweijie
  */
+
+
+static int (*chunking)(unsigned char* buf, int size);
--- src/index/context_table.h
+++ src/index/context_table.h
@@ -4,3 +4,19 @@
  *  Created on: Oct 3, 2020
  *      Author: Yanweijie
  */
+#ifndef CONTEXT_TABLE_H
+#define CONTEXT_TABLE_H
+
+struct contextItem {
+    //contextid id; // context table item id
+
+	//segmentid segment_id;
+	struct segment* segment_ptr;
+	int followers;
+	double score;
+};
+
+
+
+
+#endif 
