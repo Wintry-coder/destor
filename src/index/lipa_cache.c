f53d120dc89bb6a151514d0a1078dbc6a39a1156
Wintry-coder <1332765311@qq.com>
2020-10-16 16:18:03 +0800

    update

--- src/index/lipa_cache.c
+++ src/index/lipa_cache.c
@@ -48,3 +48,36 @@ void feedback(struct segmentRecipe* sr, char* feature) {
         }
     }
 }
+int lipa_cache_check_id(struct LIPA_cacheItem* cache, int*id) {
+    return cache ->id == *id;
+}
+struct LIPA_cacheItem* new_lipa_cache_item(struct contextItem* ctxtTableItem, struct segmentRecipe* sr) {
+    struct LIPA_cacheItem* new_cacheItem = (struct LIPA_cacheItem*)(malloc(sizeof(struct LIPA_cacheItem)));
+
+    new_cacheItem ->id = ctxtTableItem ->id;
+    new_cacheItem ->hit_score = 0;
+    new_cacheItem ->flag = 0;
+    new_cacheItem ->tableItemPtr = ctxtTableItem;
+
+    new_cacheItem ->kvpairs = g_hash_table_new_full(g_feature_hash, g_feature_equal, NULL, NULL);
+    if (sr == NULL) {
+
+        GSequenceIter* chunkIter = g_sequence_get_begin_iter(ctxtTableItem->segment_ptr->chunks);
+        GSequenceIter* chunkEnd = g_sequence_get_end_iter(ctxtTableItem->segment_ptr->chunks);
+
+        for(; chunkIter != chunkEnd; chunkIter = g_sequence_iter_next(chunkIter)){
+            struct chunk* c = g_sequence_get(chunkIter);
+            g_hash_table_insert(new_cacheItem->kvpairs, &c->fp, TEMPORARY_ID);
+        }
+
+    }else {
+        GHashTableIter tableIter;
+        gpointer key, value;
+        g_hash_table_iter_init(&tableIter, sr->kvpairs);
+        while (g_hash_table_iter_next(&tableIter, &key, &value)) {
+            g_hash_table_insert(new_cacheItem->kvpairs, &key, ((struct chunkPointer*) value)->id);
+        }
+
+    }
+    return new_cacheItem;
+}
--- src/index/lipa_cache.h
+++ src/index/lipa_cache.h
@@ -10,6 +10,27 @@
 #include "context_table.h"
 #include "../recipe/recipestore.h"
 int ItemId;
+struct LIPA_cacheItem {
+	// id is ctxtTableItem id
+	int64_t id;
+
+	struct contextItem* tableItemPtr;
+
+	// k is fingerprint and value is container id
+	// kvpairs records every chunk in segment its fingerprint and container id
+	// each cache item means a segment
+	// when feature has no corresponding container id , it will be TEMPORARY_ID otherwise it will be the container id
+	GHashTable* kvpairs;
+
+	// hit_score records the segment hit counts
+	int hit_score;
+
+	//flag means it is champion or followers
+	int flag;
+};
+
 void feedback(struct segmentRecipe* sr, char* feature);
+int lipa_cache_check_id(struct LIPA_cacheItem* cache, int*id);
+struct LIPA_cacheItem* new_lipa_cache_item(struct contextItem* ctxtTableItem, struct segmentRecipe* sr);
 
 #endif
